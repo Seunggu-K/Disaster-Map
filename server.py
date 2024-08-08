3ea3ea182c7ba5fca99051591bc3ed1f5d79a1b7
moonlight <ksg8927@gmail.com>
2024-08-09 00:31:41 +0900

    First commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import pandas as pd
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('test.html')
+
+@app.route('/api')
+def hello_world_api():
+    # SQLite 데이터베이스 연결 생성(파일이 없으면 생성)
+    conn = sqlite3.connect('D:\main.db')
+
+    # 연결 속성 설정
+    conn.text_factory = str  # 기본값이 str이며, 이는 UTF-8로 인코딩됨을 의미합니다.
+
+    # SQL 쿼리를 사용하여 데이터를 DataFrame으로 로드
+    query = "SELECT * FROM location_data"
+    df = pd.read_sql_query(query, conn)
+
+    return df.to_json(orient="table")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
